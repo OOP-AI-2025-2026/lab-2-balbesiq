8a022a6ae506468e56aea16dac76537122d8c5e4
yelchaninova <dashayelch2307@gmail.com>
2025-09-10 18:44:29 +0300

    Changed by Local Git

--- src/main/java/ua/opnu/BankAccount.java
+++ src/main/java/ua/opnu/BankAccount.java
@@ -3,10 +3,10 @@ package ua.opnu;
 public class BankAccount {
     String name;
     double balance;
-    double transactionFee;
+    double transactionFee = 0;
 
     void deposit(double amount) {
-        // TODO: modify method body
+        if(amount>0)
         balance = balance + amount;
     }
 
@@ -15,13 +15,25 @@ public class BankAccount {
     }
 
     boolean withdraw(double amount) {
-        // TODO: modify method body
-        balance = balance - amount;
-        return true;
+        if (amount <= 0) {
+            return false;
+        }
+        if (balance >= amount + transactionFee) {
+            balance -= amount + transactionFee;
+            return true;
+        }
+        return false;
     }
 
     boolean transfer(BankAccount receiver, double amount) {
-        // TODO: modify method body
+        if (amount <= 0) {
+            return false;
+        }
+        if (balance >= amount + transactionFee) {
+            balance -= amount + transactionFee;
+            receiver.balance += amount;
+            return true;
+        }
         return false;
     }
 }
